d3ac4a6bd91527935fa9d3042101a7d55c433530
Surjith <mahindhiran005@gmail.com>
2024-04-18 10:19:00 +0530

    added dotenv and connect mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("DB CONNECTD"))
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server Listening on 3000");
 });
